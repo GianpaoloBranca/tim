95442ba8be8188598579a0b47e27ae73f96cf516
GianBra <gianpaolo@protonmail.com>
2016-06-07 08:49:25 +0200

    page devices and php doc conversion

---
+++ devices.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <?php include 'stylesheet.php'; ?>
+    <title>Devices</title>
+  </head>
+  <body>
+    <?php include "header.php"; ?>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="pull-right">
+          <img src="img/banner_blocchetto.jpg" alt=""/>
+        </div>
+        <div class="indent">
+          <p class="very-large">We have everything you need!</p>
+          <p class="h3">Have a look to our special selection of discounted devices! ->
+            <a class="btn btn-default" href="#" role="button">Of Course!</a>
+          </p>
+          <h4>Or just click on one of the panel below to search something specific...</h4>
+        </div>
+      </div>
+      <div class="col-md-3 bordered">
+        <img src="img/smartphones.jpg" width="100%" height="187px" alt="" />
+        <p class="text-center">
+          smartphones
+        </p>
+      </div>
+      <div class="col-md-3 bordered">
+        <img src="img/tablets.jpg" width="100%" height="187px" alt="" />
+        <p class="text-center">
+          tablets
+        </p>
+      </div>
+      <div class="col-md-3 bordered">
+        <img src="img/modems.png" width="100%" height="187px" alt="" />
+        <p class="text-center">
+          modem and networking
+        </p>
+      </div>
+      <div class="col-md-3 bordered">
+        <img src="img/landlines.jpg" width="100%" height="187px" alt="" />
+        <p class="text-center">
+          landlines
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ testimonials.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <?php include "stylesheet.php"; ?>
+    <title>Testimonials</title>
+  </head>
+  <body>
+    <?php include "header.php"; ?>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-1 col-md-offset-1">
+          <a class="btn btn-info" href="#" role="button">Innovation</a>
+        </div>
+        <div class="col-md-8">
+          <p class="h3 text-center">
+            Testimonials
+            <br>
+            <br>
+          </p>
+        </div>
+        <div class="col-md-2">
+          <a class="btn btn-info" href="#" role="button">Projects</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="pull-right crop-square" align="center">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Tim_Berners-Lee-Knight-crop.jpg" width="100%">
+          </div>
+          <div class="pull-right" style="width:300px">
+            <br>
+          </div>
+          <div class="pull-right" style="width:300px;border:2px solid black">
+            <p class="h4 text-center">
+              "When you look at the web as if you observe the whole humanity"
+            </p>
+            <p class="h5 text-center">
+              Tim Berners-Lee
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="center-block crop-square">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/Fabio_Fazio_cropped.jpg" width="100%">
+          </div>
+          <br>
+          <div class="center-block" style="width:300px;border:2px solid black">
+            <p class="h4 text-center">
+              "The future will surprice us"
+            </p>
+            <p class="h5 text-center">
+              Fabio Fazio
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="pull-left crop-square">
+            <img src="http://www.ansa.it/webimages/img_457x/2014/2/23/a5b0d6dff71ef480fcc6be5e88c92f91.jpg" width="100%">
+          </div>
+          <div class="pull-left" style="width:300px">
+            <br>
+          </div>
+          <div class="pull-left" style="width:300px;border:2px solid black">
+            <p class="h4 text-center">
+              "We hunger for wonder!"
+            </p>
+            <p class="h5 text-center">
+              Pif
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
