d175ba62c7086d004058ef0debc3bc4db5b1826b
GianBra <gianpaolo@protonmail.com>
2016-06-11 21:14:30 +0200

    the group page created

---
+++ the-group.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <?php include 'stylesheet.php'; ?>
+    <title>Devices</title>
+  </head>
+<body>
+  <?php include "header.php"; ?>
+  <div class="row">
+    <div class="row-height">
+      <div class="col-md-3 col-height col-top" style="background-color:#2B60DE">
+        <div class="inside">
+          <a href="#" class="btn btn-default">News</a>
+          <br>
+          <a href="#" class="btn btn-default">Business and Market</a>
+        </div>
+      </div>
+      <div class="col-md-9 col-height">
+        <div class="inside">
+          <p class="text-center h1">
+            <i>"Telecom Italy, a partnership <br>committed to building the future of Italy"</i>
+          </p>
+          <p class="text-center h4">
+            -Giuseppe Recchi, president <br><br>
+            <img src="img/pannellisolari.png" alt="" />
+          </p>
+          <p class="large">
+            <br>
+            <b>Description: <br></b>
+            In the Manifesto of TIM and his new position the keys to understanding the vision
+            of the Telecom Italia group today <br>
+            <br>
+            <b>Communication is freedom. Freedom to communicate. <br></b>
+            In the twenty-first century only those whocan share ideas and experience are really free to pursue
+            their aspirations. And to think big. And only a group that think bigcan actually promote progress
+            and prosperity, ensuring the full freedom to communicate. Always. Everywhere. Better <br>
+            <br>
+            <b>Connected to life. Always. Everywhere. Better. <br></b>
+            We put the best landline and mobile networkso serve the needs of any person and any organization,
+            ensuring the best quality and safety in the experience of content, supported by a competentand experienced
+            services, accessible across all online and offline channels <br>
+            <br>
+            <b>Governance: <br></b>
+            <img src="img/sistema-governance-maggio-2016-ita.jpg" alt="" width="100%"/>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
