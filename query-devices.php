647554041e6df504fe4ef797ad12251077777b1c
GianBra <gianpaolo@protonmail.com>
2016-07-15 21:09:46 +0200

    uploaded all files from alter vista

---
+++ query-devices.php
@@ -0,0 +1,28 @@
+<?php
+
+$type = $_GET['type'];
+
+$username = "timwebs";
+$password = "";
+$hostname = "localhost";
+
+$dbhandle = mysqli_connect($hostname, $username, $password)
+  or die( json_encode(array( "error"=> "Impossibile connettersi al server.") ) );
+$selected = mysqli_select_db($dbhandle, "my_timwebs")
+  or die( json_encode(array("error"=> "Errore di connessione al server.") ) );
+
+$string = "SELECT * FROM Devices WHERE (type='".$type."')";
+$query = mysqli_query($dbhandle, $string);
+$data = array();
+while($row = mysqli_fetch_array($query)) {
+	$data[] = array(
+    	"id" => $row["id"],
+    	"name" => $row["name"],
+        "price" => $row["price"]
+    );
+}
+echo( json_encode( $data ) );
+
+mysqli_close($dbhandle);
+
+?>
